25b7826701f334b760405eca885c4bbc12bd645f
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 21:36:22 +0300

    Prepare display facing patch dependencies

--- src/main/java/com/foreverspark/logicsim/mixin/DisplayFacingPatch.java
+++ src/main/java/com/foreverspark/logicsim/mixin/DisplayFacingPatch.java
@@ -1,7 +1,17 @@
 package com.foreverspark.logicsim.mixin;
 
+import com.foreverspark.logicsim.block.DisplayBlock;
 import com.foreverspark.logicsim.block.DisplayBlockEntity;
+import com.foreverspark.logicsim.interconnect.CableRuntime;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DisplayBlockEntity.class)
 public abstract class DisplayFacingPatch {
