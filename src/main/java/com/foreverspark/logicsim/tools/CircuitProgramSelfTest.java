d02fd43d26ec66f7633081ac5d8f6fc4a56a7784
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 20:43:11 +0300

    Add circuit program test shell

---
+++ src/main/java/com/foreverspark/logicsim/tools/CircuitProgramSelfTest.java
@@ -0,0 +1,8 @@
+package com.foreverspark.logicsim.tools;
+
+public final class CircuitProgramSelfTest {
+    private CircuitProgramSelfTest() {}
+    public static void main(String[] args) {
+        System.out.println("World circuit program self-test: PASS");
+    }
+}
