5baa83461dd008a1e0d2f1c41d465fcccc9fb03e
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 15:58:25 +0300

    Extend editor ports

--- src/main/java/com/foreverspark/logicsim/editor/model/NodePorts.java
+++ src/main/java/com/foreverspark/logicsim/editor/model/NodePorts.java
@@ -9,13 +9,13 @@ public final class NodePorts {
 
     public static List<PortSpec> inputs(EditorNode node, ChipLookup chips) {
         return switch (node.kind) {
-            case INPUT -> List.of();
+            case INPUT, CONSTANT -> List.of();
             case OUTPUT -> List.of(new PortSpec("IN", PortDirection.INPUT, node.width));
             case NAND -> List.of(
                     new PortSpec("A", PortDirection.INPUT, 1),
                     new PortSpec("B", PortDirection.INPUT, 1)
             );
-            case SPLITTER -> List.of(new PortSpec("BUS", PortDirection.INPUT, node.width));
+            case PROBE, BUS, SPLITTER -> List.of(new PortSpec("BUS", PortDirection.INPUT, node.width));
             case MERGER -> bitPorts("B", PortDirection.INPUT, node.width);
             case CUSTOM_CHIP -> requireChip(node, chips).inputPorts();
         };
@@ -24,8 +24,10 @@ public final class NodePorts {
     public static List<PortSpec> outputs(EditorNode node, ChipLookup chips) {
         return switch (node.kind) {
             case INPUT -> List.of(new PortSpec("OUT", PortDirection.OUTPUT, node.width));
-            case OUTPUT -> List.of();
+            case OUTPUT, PROBE -> List.of();
             case NAND -> List.of(new PortSpec("OUT", PortDirection.OUTPUT, 1));
+            case CONSTANT -> List.of(new PortSpec("OUT", PortDirection.OUTPUT, node.width));
+            case BUS -> List.of(new PortSpec("BUS", PortDirection.OUTPUT, node.width));
             case SPLITTER -> bitPorts("B", PortDirection.OUTPUT, node.width);
             case MERGER -> List.of(new PortSpec("BUS", PortDirection.OUTPUT, node.width));
             case CUSTOM_CHIP -> requireChip(node, chips).outputPorts();
