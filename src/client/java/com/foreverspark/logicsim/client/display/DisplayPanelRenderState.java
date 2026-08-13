32597fe4a3b3314b4cca008c06fc618426fb0342
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 18:50:23 +0300

    Add display render state

---
+++ src/client/java/com/foreverspark/logicsim/client/display/DisplayPanelRenderState.java
@@ -0,0 +1,9 @@
+package com.foreverspark.logicsim.client.display;
+
+import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
+
+public final class DisplayPanelRenderState extends BlockEntityRenderState {
+    public int width;
+    public int height;
+    public int[] pixelsArgb = new int[0];
+}
