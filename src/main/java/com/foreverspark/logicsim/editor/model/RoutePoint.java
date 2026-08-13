30574cde6909d6dd3bc3a2eedf3a80561aa89929
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 13:14:46 +0300

    Add wire route point model

---
+++ src/main/java/com/foreverspark/logicsim/editor/model/RoutePoint.java
@@ -0,0 +1,5 @@
+package com.foreverspark.logicsim.editor.model;
+
+/** A manually editable orthogonal wire-routing point in circuit world coordinates. */
+public record RoutePoint(double x, double y) {
+}
