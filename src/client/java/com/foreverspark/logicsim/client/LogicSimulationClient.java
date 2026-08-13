d8086d64b2af5e0f85f11d544e3f4b6864dbecfa
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 18:51:09 +0300

    Register display panel renderer

--- src/client/java/com/foreverspark/logicsim/client/LogicSimulationClient.java
+++ src/client/java/com/foreverspark/logicsim/client/LogicSimulationClient.java
@@ -1,9 +1,12 @@
 package com.foreverspark.logicsim.client;
 
+import com.foreverspark.logicsim.block.entity.ModBlockEntities;
+import com.foreverspark.logicsim.client.display.DisplayPanelRenderer;
 import com.foreverspark.logicsim.client.screen.CircuitEditorScreen;
 import com.foreverspark.logicsim.platform.ClientEditorBridge;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 
 public final class LogicSimulationClient implements ClientModInitializer {
     @Override
@@ -11,5 +14,6 @@ public final class LogicSimulationClient implements ClientModInitializer {
         ClientEditorBridge.installEditorOpener(() ->
                 Minecraft.getInstance().gui.setScreen(new CircuitEditorScreen())
         );
+        BlockEntityRenderers.register(ModBlockEntities.DISPLAY_PANEL, DisplayPanelRenderer::new);
     }
 }
