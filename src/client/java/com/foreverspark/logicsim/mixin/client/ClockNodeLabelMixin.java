39937decdf35b41b3edc6ac4f4c3120e1776a816
snwaf929-cpu <snwaf929@gmail.com>
2026-08-14 01:03:13 +0300

    Prepare compact clock label renderer

--- src/client/java/com/foreverspark/logicsim/mixin/client/ClockNodeLabelMixin.java
+++ src/client/java/com/foreverspark/logicsim/mixin/client/ClockNodeLabelMixin.java
@@ -1,5 +1,17 @@
 package com.foreverspark.logicsim.mixin.client;
 
-public final class ClockNodeLabelMixin {
-    private ClockNodeLabelMixin() {}
+import com.foreverspark.logicsim.client.screen.CircuitCanvasWidget;
+import com.foreverspark.logicsim.editor.model.CircuitDocument;
+import com.foreverspark.logicsim.editor.model.EditorNode;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(value = CircuitCanvasWidget.class, priority = 1450)
+public abstract class ClockNodeLabelMixin {
+    @Shadow private CircuitDocument document;
+    @Shadow private double zoom;
+    @Shadow private int screenX(double x) { throw new AssertionError(); }
+    @Shadow private int screenY(double y) { throw new AssertionError(); }
+    @Shadow private double nodeWidth(EditorNode node) { throw new AssertionError(); }
 }
