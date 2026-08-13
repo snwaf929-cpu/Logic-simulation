8386bbd8407e7ec493df87a8fd95e6f5961d9cc7
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 21:17:10 +0300

    Open Circuit IO selector from connector blocks

--- src/client/java/com/foreverspark/logicsim/client/CircuitBlockUseHandler.java
+++ src/client/java/com/foreverspark/logicsim/client/CircuitBlockUseHandler.java
@@ -1,7 +1,10 @@
 package com.foreverspark.logicsim.client;
 
+import com.foreverspark.logicsim.block.ConnectorBlocks;
 import com.foreverspark.logicsim.block.ModBlocks;
+import com.foreverspark.logicsim.network.RequestCircuitPortsPayload;
 import com.foreverspark.logicsim.platform.ClientEditorBridge;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -10,11 +13,19 @@ public final class CircuitBlockUseHandler {
     private CircuitBlockUseHandler() {}
 
     public static void register() {
+        ClientPortNetworking.initialize();
         UseBlockCallback.EVENT.register((player, level, hand, hit) -> {
             if (hand != InteractionHand.MAIN_HAND) return InteractionResult.PASS;
-            if (level.getBlockState(hit.getBlockPos()).getBlock() != ModBlocks.CIRCUIT_BLOCK) return InteractionResult.PASS;
-            ClientEditorBridge.openEditor(hit.getBlockPos());
-            return InteractionResult.SUCCESS;
+            var block = level.getBlockState(hit.getBlockPos()).getBlock();
+            if (block == ModBlocks.CIRCUIT_BLOCK) {
+                ClientEditorBridge.openEditor(hit.getBlockPos());
+                return InteractionResult.SUCCESS;
+            }
+            if (block == ConnectorBlocks.IO_CONNECTOR) {
+                ClientPlayNetworking.send(new RequestCircuitPortsPayload(hit.getBlockPos()));
+                return InteractionResult.SUCCESS;
+            }
+            return InteractionResult.PASS;
         });
     }
 }
