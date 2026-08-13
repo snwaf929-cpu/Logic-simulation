518404adc59185dd168f11ed96faff00b89e81c7
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 19:23:00 +0300

    Seed built-in display peripheral

--- src/client/java/com/foreverspark/logicsim/client/LogicSimulationClient.java
+++ src/client/java/com/foreverspark/logicsim/client/LogicSimulationClient.java
@@ -1,15 +1,34 @@
 package com.foreverspark.logicsim.client;
 
+import com.foreverspark.logicsim.LogicSimulationMod;
+import com.foreverspark.logicsim.client.chip.ClientChipLibrary;
+import com.foreverspark.logicsim.client.device.BuiltinDevices;
 import com.foreverspark.logicsim.client.screen.CircuitEditorScreen;
+import com.foreverspark.logicsim.editor.model.ChipDefinition;
 import com.foreverspark.logicsim.platform.ClientEditorBridge;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.Minecraft;
 
+import java.io.IOException;
+
 public final class LogicSimulationClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ensureBuiltInDevices();
         ClientEditorBridge.installEditorOpener(() ->
                 Minecraft.getInstance().gui.setScreen(new CircuitEditorScreen())
         );
+    }
+
+    private static void ensureBuiltInDevices() {
+        try {
+            ClientChipLibrary library = new ClientChipLibrary();
+            if (!library.exists(BuiltinDevices.DISPLAY)) {
+                ChipDefinition display = BuiltinDevices.find(BuiltinDevices.DISPLAY);
+                library.save(display.name, display.circuit, display.color, display.visual, "");
+            }
+        } catch (IOException | RuntimeException exception) {
+            LogicSimulationMod.LOGGER.error("Could not install built-in display peripheral", exception);
+        }
     }
 }
