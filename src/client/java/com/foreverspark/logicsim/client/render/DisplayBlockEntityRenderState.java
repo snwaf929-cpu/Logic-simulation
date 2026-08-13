5e53cb043d23d9346cc26ef3daec735c1abc8f3a
snwaf929-cpu <snwaf929@gmail.com>
2026-08-14 02:59:25 +0300

    Remove obsolete display render state

--- src/client/java/com/foreverspark/logicsim/client/render/DisplayBlockEntityRenderState.java
+++
@@ -1,8 +0,0 @@
-package com.foreverspark.logicsim.client.render;
-
-import com.foreverspark.logicsim.block.DisplayBlockEntity;
-import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-
-public final class DisplayBlockEntityRenderState extends BlockEntityRenderState {
-    public final int[] pixels = new int[DisplayBlockEntity.WIDTH * DisplayBlockEntity.HEIGHT];
-}
