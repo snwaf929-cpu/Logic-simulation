faf57e8be4bdc120f45d3ebfb6d90d0f5669b3bf
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 20:52:56 +0300

    Handle Circuit Block client use

--- src/client/java/com/foreverspark/logicsim/client/CircuitBlockUseHandler.java
+++ src/client/java/com/foreverspark/logicsim/client/CircuitBlockUseHandler.java
@@ -1,6 +1,20 @@
 package com.foreverspark.logicsim.client;
 
+import com.foreverspark.logicsim.block.ModBlocks;
+import com.foreverspark.logicsim.platform.ClientEditorBridge;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+
 public final class CircuitBlockUseHandler {
     private CircuitBlockUseHandler() {}
-    public static void register() {}
+
+    public static void register() {
+        UseBlockCallback.EVENT.register((player, level, hand, hit) -> {
+            if (hand != InteractionHand.MAIN_HAND) return InteractionResult.PASS;
+            if (level.getBlockState(hit.getBlockPos()).getBlock() != ModBlocks.CIRCUIT_BLOCK) return InteractionResult.PASS;
+            ClientEditorBridge.openEditor(hit.getBlockPos());
+            return InteractionResult.SUCCESS;
+        });
+    }
 }
