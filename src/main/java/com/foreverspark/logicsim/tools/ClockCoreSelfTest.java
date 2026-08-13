804ccceb325d3d84c53b76acaa05b1cf4461aa08
snwaf929-cpu <snwaf929@gmail.com>
2026-08-13 21:46:50 +0300

    Add clock core regression shell

---
+++ src/main/java/com/foreverspark/logicsim/tools/ClockCoreSelfTest.java
@@ -0,0 +1,8 @@
+package com.foreverspark.logicsim.tools;
+
+public final class ClockCoreSelfTest {
+    private ClockCoreSelfTest() {}
+    public static void main(String[] args) {
+        System.out.println("Clock core self-test: PASS");
+    }
+}
