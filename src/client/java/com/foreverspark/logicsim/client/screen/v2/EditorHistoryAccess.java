f75e95386ea71596b38ba76cd098c252391b43aa
snwaf929-cpu <snwaf929@gmail.com>
2026-08-15 07:17:52 +0300

    Expose history transaction commit

--- src/client/java/com/foreverspark/logicsim/client/screen/v2/EditorHistoryAccess.java
+++ src/client/java/com/foreverspark/logicsim/client/screen/v2/EditorHistoryAccess.java
@@ -3,6 +3,7 @@ package com.foreverspark.logicsim.client.screen.v2;
 /** Implemented onto CircuitCanvasWidget by the Editor V2 interaction mixin. */
 public interface EditorHistoryAccess {
     void logic$checkpoint(String label);
+    void logic$commitHistory();
     boolean logic$undo();
     boolean logic$redo();
 }
