9c2a0b3c56a5e85f47f3fe9528e92752075af631
snwaf929-cpu <snwaf929@gmail.com>
2026-08-15 21:10:27 +0300

    Remove obsolete built-in display chip installer

--- src/client/java/com/foreverspark/logicsim/client/LogicSimulationClient.java
+++ src/client/java/com/foreverspark/logicsim/client/LogicSimulationClient.java
@@ -1,26 +1,19 @@
 package com.foreverspark.logicsim.client;
 
-import com.foreverspark.logicsim.LogicSimulationMod;
 import com.foreverspark.logicsim.block.ModBlockEntities;
-import com.foreverspark.logicsim.client.chip.ClientChipLibrary;
-import com.foreverspark.logicsim.client.device.BuiltinDevices;
 import com.foreverspark.logicsim.client.render.DisplayBlockEntityRenderer;
 import com.foreverspark.logicsim.client.render.DisplayTextureCache;
 import com.foreverspark.logicsim.client.render.RealtimeDisplayTextureCache;
 import com.foreverspark.logicsim.client.screen.CircuitEditorScreen;
-import com.foreverspark.logicsim.editor.model.ChipDefinition;
 import com.foreverspark.logicsim.platform.ClientEditorBridge;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 
-import java.io.IOException;
-
 public final class LogicSimulationClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ensureBuiltInDevices();
         BlockEntityRenderers.register(ModBlockEntities.DISPLAY, DisplayBlockEntityRenderer::new);
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             DisplayTextureCache.clientTick();
@@ -29,17 +22,5 @@ public final class LogicSimulationClient implements ClientModInitializer {
         ClientEditorBridge.installEditorOpener(() -> Minecraft.getInstance().gui.setScreen(new CircuitEditorScreen()));
         ClientBoardNetworking.initialize();
         CircuitBlockUseHandler.register();
-    }
-
-    private static void ensureBuiltInDevices() {
-        try {
-            ClientChipLibrary library = new ClientChipLibrary();
-            if (!library.exists(BuiltinDevices.DISPLAY)) {
-                ChipDefinition display = BuiltinDevices.find(BuiltinDevices.DISPLAY);
-                library.save(display.name, display.circuit, display.color, display.visual, "");
-            }
-        } catch (IOException | RuntimeException exception) {
-            LogicSimulationMod.LOGGER.error("Could not install built-in display peripheral", exception);
-        }
     }
 }
