8044b57051711e080e9355d21077017937e60173
snwaf929-cpu <snwaf929@gmail.com>
2026-08-16 06:04:12 +0300

    Add reusable CHIP replacement editor access

---
+++ src/client/java/com/foreverspark/logicsim/client/screen/v2/CustomChipReplacementAccess.java
@@ -0,0 +1,6 @@
+package com.foreverspark.logicsim.client.screen.v2;
+
+/** Replace one selected reusable CHIP instance with the currently chosen library CHIP. */
+public interface CustomChipReplacementAccess {
+    boolean logic$replaceSelectedWithPlacementChip();
+}
